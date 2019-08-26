fe04b4262f049a8c58d0f633ec42388257aca7c3
Alex Rabelo Ferreira <arf92@live.com>
2019-08-26 19:26:54 -0300

    Add dialog

--- projetos_activity_life_cycle/TestActivityLifeCycle1/app/src/main/java/br/senac/go/testactivitylifecycle1/MainActivity.java
+++ projetos_activity_life_cycle/TestActivityLifeCycle1/app/src/main/java/br/senac/go/testactivitylifecycle1/MainActivity.java
@@ -1,7 +1,9 @@
 package br.senac.go.testactivitylifecycle1;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +20,18 @@ public class MainActivity extends BaseActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button openDialogBT = findViewById(R.id.bt_open_dialog);
+        openDialogBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("Teste de dialog")
+                        .setCancelable(true)
+                        .setNegativeButton("OK", null)
+                        .setMessage("Veja o log agora, antes de fechar o dialog")
+                        .create().show();
             }
         });
     }
