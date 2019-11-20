dac83a3ba6a3ad716433e33d9ef0da8dd7a17758
Alex Ferreira <arf92@live.com>
2019-11-20 18:58:04 -0300

    Adicao de projetos de outras branches em master

---
+++ projetos_activity_life_cycle/TestActivityLifeCycle1/app/src/main/java/br/senac/go/testactivitylifecycle1/Main2Activity.java
@@ -0,0 +1,25 @@
+package br.senac.go.testactivitylifecycle1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button openBT = findViewById(R.id.bt_open);
+        openBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
